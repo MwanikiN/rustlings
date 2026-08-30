f0362dc88d874270350e840c00bc7424b598d7bf
neemamwaniki <neema.mwaniki@4g-capital.com>
2026-08-30 22:46:09 +0300

    (chore): modified vec type files

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,7 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        output.push(element * 2);
     }
 
     output
@@ -24,13 +25,21 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+            element * 2
         })
         .collect()
 }
 
 fn main() {
-    // You can optionally experiment here.
+    let input = [2, 4, 6, 8, 10];
+    let ans = vec_loop(&input);
+    println!("vec_loop: {:?}", ans);
+
+    let ans = vec_map_example(&input);
+    println!("vec_map_example: {:?}", ans);
+
+    let ans = vec_map(&input);
+    println!("vec_map: {:?}", ans);
 }
 
 #[cfg(test)]
