8aebedb4a80a69f84b1de79456264b8a038d5691
neemamwaniki <neema.mwaniki@4g-capital.com>
2026-09-06 12:56:17 +0300

    (chore): modified options1 function

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,10 +4,20 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day > 22 {
+        None
+    } else {
+        Some(5)
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let ice_creams = maybe_ice_cream(12);
+    println!("{:?}", ice_creams);
+
+    let ice_creams = maybe_ice_cream(23);
+    println!("{:?}", ice_creams);
 }
 
 #[cfg(test)]
@@ -18,7 +28,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let ice_creams = maybe_ice_cream(12);
+        let ice_creams = maybe_ice_cream(12).unwrap();
 
         assert_eq!(ice_creams, 5); // Don't change this line.
     }
