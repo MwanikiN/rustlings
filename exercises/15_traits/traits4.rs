44ab25a4edcc9a175b2b071d03dec80fdf67769f
neemamwaniki <neema.mwaniki@4g-capital.com>
2026-09-07 16:42:07 +0300

    (refactor): refactored traits files to include bound

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -1,6 +1,6 @@
 trait Licensed {
     fn licensing_info(&self) -> String {
-        "Default license".to_string()
+        "Default license".to_string() //returns a string
     }
 }
 
@@ -11,12 +11,22 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+fn compare_license_types(software1: impl Licensed, software2: impl Licensed) -> bool {
+    software1.licensing_info() == software2.licensing_info()
+}
+
+//alternatively using generics
+fn compare_license_types_generic<T: Licensed, U: Licensed>(software1: T, software2: U) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
 
 fn main() {
     // You can optionally experiment here.
+    assert!(compare_license_types_generic(SomeSoftware, OtherSoftware));
+    let software1 = SomeSoftware.licensing_info();
+    let software2 = OtherSoftware.licensing_info();
+    println!("SomeSoftware licensing info: {software1}");
+    println!("OtherSoftware licensing info: {software2}");
 }
 
 #[cfg(test)]
--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -10,21 +10,31 @@ trait OtherTrait {
     }
 }
 
-struct SomeStruct;
+struct SomeStruct; //Unit struct. Recall they have 0 bytes.
 impl SomeTrait for SomeStruct {}
 impl OtherTrait for SomeStruct {}
 
-struct OtherStruct;
+struct OtherStruct; //Unit struct as well. Check documentation on implementation and use cases.
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool { //both traits are required to be implemented for the function to work. This is called a trait bound. The function will only accept items that implement both traits.
+    item.some_function() && item.other_function()
+}
+
+//alternatively lets use generics
+fn some_func_generic<T: SomeTrait + OtherTrait>(item: T) -> bool {
     item.some_function() && item.other_function()
 }
 
 fn main() {
     // You can optionally experiment here.
+    //implementing the above traits to test them out 
+    let some_struct = SomeStruct;
+    let other_struct = OtherStruct;
+    println!("SomeStruct: {}", some_func_generic(some_struct));
+    println!("OtherStruct: {}", some_func(other_struct));
 }
 
 #[cfg(test)]
