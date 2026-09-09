1c666c48315a2b79359553dd07e9561030414cb2
neemamwaniki <neema.mwaniki@4g-capital.com>
2026-09-09 10:31:45 +0300

    feat): tinkered with tests1

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -6,18 +6,24 @@ fn is_even(n: i64) -> bool {
 }
 
 fn main() {
-    // You can optionally experiment here.
+    let number = 4;
+    if is_even(number) {
+        println!("{} is even", number);
+    } else {    
+        println!("{} is odd", number);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     // TODO: Import `is_even`. You can use a wildcard to import everything in
     // the outer module.
+    use super::*;
 
     #[test]
     fn you_can_assert() {
         // TODO: Test the function `is_even` with some values.
-        assert!();
-        assert!();
+        assert!(is_even(4));
+        assert!(!is_even(3));
     }
 }
