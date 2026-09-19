224769c3ed8665f91594458145b9d4499dc85451
neemamwaniki <neema.mwaniki@4g-capital.com>
2026-09-19 15:30:20 +0300

    feat(clippy): resolved lint issues highlighted by clippy

--- exercises/22_clippy/clippy2.rs
+++ exercises/22_clippy/clippy2.rs
@@ -2,7 +2,10 @@ fn main() {
     let mut res = 42;
     let option = Some(12);
     // TODO: Fix the Clippy lint.
-    for x in option {
+    // for x in option {
+    //     res += x;
+    // } changing this block to use if let instead
+    if let Some(x) = option {
         res += x;
     }
 
